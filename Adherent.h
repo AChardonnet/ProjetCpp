c86b07e05707371a4b0a9455161f7e175c123bf9
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-01-07 08:46:00 +0100

    created more classes

---
+++ Adherent.h
@@ -0,0 +1,16 @@
+//
+// Created by antoi on 07/01/2025.
+//
+
+#ifndef ADHERENT_H
+#define ADHERENT_H
+
+
+
+class Adherent {
+
+};
+
+
+
+#endif //ADHERENT_H
---
+++ Bibliotheque.h
@@ -0,0 +1,16 @@
+//
+// Created by antoi on 07/01/2025.
+//
+
+#ifndef BIBLIOTHEQUE_H
+#define BIBLIOTHEQUE_H
+
+
+
+class Bibliotheque {
+
+};
+
+
+
+#endif //BIBLIOTHEQUE_H
