d9ce8c9f301d4daa9835ae8ed1c38f33d7b40100
AGachelin <159674898+AGachelin@users.noreply.github.com>
2025-01-07 11:43:41 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 #include "Adherent.h"
+#include "Album.h"
+#include "BD.h"
+#include "Bibliotheque.h"
+#include "Recueil.h"
+#include "Roman.h"
+#include "Theatre.h"
 
 using namespace std;
 
