9358653965e8433c847aeecfa5047e1257a3d062
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-01-07 09:47:31 +0100

    Class Album

--- Album.cpp
+++ Album.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Album.h"
+
+Album::Album() : Livre() {
+}
+
+Album::Album(int id, string auteur, string titre, string editeur, int ISBN, string publicCible, string etat,
+             string illustrations) : Livre(id, auteur, titre, editeur, ISBN, publicCible, etat) {
+    this->illustrations = illustrations;
+}
--- Album.h
+++ Album.h
@@ -4,13 +4,17 @@
 
 #ifndef ALBUM_H
 #define ALBUM_H
+#include "Livre.h"
 
 
+class Album : Livre {
+private:
+    string illustrations;
 
-class Album {
-
+public:
+    Album();
+    Album(int id, string auteur, string titre, string editeur, int ISBN, string public_cible, string etat, string illustrations);
 };
-
 
 
 #endif //ALBUM_H
